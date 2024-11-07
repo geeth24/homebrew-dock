1a718933d928cdd67500c6ca4f67d2c03de21270
Geeth Gunnampalli <geeth.gunnampalli@gmail.com>
2024-11-07 11:45:35 -0600

    Refactor Dock formula to remove virtualenv inclusion and install dependencies from requirements.txt

--- dock.rb
+++ dock.rb
@@ -1,6 +1,4 @@
 class Dock < Formula
-  include Language::Python::Virtualenv
-
   desc "Dock: A custom Docker CLI tool for managing Docker Compose environments"
   homepage "https://github.com/geeth24/dock"
   url "https://github.com/geeth24/dock/archive/v1.0.0.tar.gz"
@@ -9,12 +7,12 @@ class Dock < Formula
 
   depends_on "python@3.9"
 
-  resource "pyyaml" do
-    url "https://files.pythonhosted.org/packages/7d/e1/95a7b1d382b474f4d9ab7cbb6e6b8a6be84c5e282e1d29010745dc0c5e98/PyYAML-5.4.1.tar.gz"
-    sha256 "607774cb05541fc3f5b8fc9c3a4ae6da1a338c6de3f84f47972e882b2d30e4c5"
-  end
-
   def install
-    virtualenv_install_with_resources
+    # Install Python dependencies from requirements.txt
+    system "pip3", "install", "-r", "requirements.txt", "--target=#{libexec}"
+    
+    # Install the main script and set the PATH
+    bin.install "dock"
+    (bin/"dock").write_env_script libexec/"dock", PATH: "#{libexec}/bin:$PATH"
   end
 end
