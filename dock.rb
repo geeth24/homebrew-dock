0f62cba277cf1d495722d3828a4e9a1438fc1ee7
Geeth Gunampalli <geeth.gunnampalli@gmail.com>
2024-11-07 17:35:47 +0000

    Update Dock to version v1.0.0

--- dock.rb
+++ dock.rb
@@ -2,7 +2,7 @@ class Dock < Formula
   desc "Dock: A custom Docker CLI tool for managing Docker Compose environments"
   homepage "https://github.com/geeth24/dock"
   url "https://github.com/geeth24/dock/archive/v1.0.0.tar.gz"
-  sha256 "f6bf307ed9df3efede8a33852913f56e9ed7a331b19e1a4fd1a8c7cb36b0151f"
+  sha256 "06d3954fb40b1563d62ab5efbce867a745122fd2823a0205971cfc99d8f33226"
   license "MIT"
 
   depends_on "python@3.9"
