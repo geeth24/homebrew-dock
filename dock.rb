699d818243c776dbdce77e9288f635b6611f75d6
Geeth Gunnampalli <geeth.gunnampalli@gmail.com>
2024-11-07 11:40:03 -0600

    Enhance Dock formula to include Python virtualenv and resource management for PyYAML

--- dock.rb
+++ dock.rb
@@ -1,4 +1,6 @@
 class Dock < Formula
+  include Language::Python::Virtualenv
+
   desc "Dock: A custom Docker CLI tool for managing Docker Compose environments"
   homepage "https://github.com/geeth24/dock"
   url "https://github.com/geeth24/dock/archive/v1.0.0.tar.gz"
@@ -7,11 +9,13 @@ class Dock < Formula
 
   depends_on "python@3.9"
 
-  def install
-    venv = virtualenv_create(libexec, "python3")
-    venv.pip_install "pyyaml"
+  resource "pyyaml" do
+    url "https://files.pythonhosted.org/packages/7d/e1/95a7b1d382b474f4d9ab7cbb6e6b8a6be84c5e282e1d29010745dc0c5e98/PyYAML-5.4.1.tar.gz"
+    sha256 "607774cb05541fc3f5b8fc9c3a4ae6da1a338c6de3f84f47972e882b2d30e4c5"
+  end
 
+  def install
+    virtualenv_install_with_resources
     bin.install "dock"
-    (bin/"dock").write_env_script libexec/"bin/dock", PATH: "#{libexec}/bin:$PATH"
   end
 end
